7aaa354ed1ced565efbc06f700386039f086e76a
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-07-18 18:42:28 +0200

    Improve resilience of ESlint config. Improve TSconfig to recognize volto-light-theme package correctly.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,27 @@ module.exports = {
   extends: `${coreLocation}/packages/volto/.eslintrc`,
   rules: {
     'import/no-unresolved': 1,
+    'import/named': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'no-restricted-imports': [
+      'error',
+      {
+        name: '@plone/volto/components',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/helpers',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/actions',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+    ],
+    'react/jsx-key': [2, { checkFragmentShorthand: true }],
   },
   settings: {
     'import/resolver': {
@@ -28,7 +49,14 @@ module.exports = {
           ['@plone/volto', `${coreLocation}/packages/volto/src`],
           ['@plone/volto-slate', `${coreLocation}/packages/volto-slate/src`],
           ['@plone/registry', `${coreLocation}/packages/registry/src`],
-          ['@kitconcept/volto-logos-block', './packages/volto-logos-block/src'],
+          [
+            '@kitconcept/volto-light-theme',
+            `${projectRootPath}/../volto-light-theme/frontend/packages/volto-light-theme/src`,
+          ],
+          [
+            '@kitconcept/volto-logos-block',
+            `${projectRootPath}/packages/volto-logos-block/src`,
+          ],
           ...addonAliases,
         ],
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
